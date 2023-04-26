1d8c416e634154e1c976ab72005d767c80350541
Yunis-Mammadovm <yunismammadovv@gmail.com>
2023-04-26 08:36:34 +0400

    task added

---
+++ index.js
@@ -0,0 +1,57 @@
+let firstDiv = document.createElement("div")
+let card1h3 = document.createElement("h3")
+let card1p = document.createElement("p")
+let card1a = document.createElement("a")
+let card2h3 = document.createElement("h3")
+let card2p = document.createElement("p")
+let card2a = document.createElement("a")
+let card3h3 = document.createElement("h3")
+let card3p = document.createElement("p")
+let card3a = document.createElement("a")
+let card1 = document.createElement("div")
+let card2 = document.createElement("div")
+let card3 = document.createElement("div")
+
+document.body.appendChild(firstDiv)
+document.body.appendChild(card1)
+document.body.appendChild(card2)
+document.body.appendChild(card3)
+document.body.appendChild(card1h3)
+document.body.appendChild(card1p)
+document.body.appendChild(card1a)
+document.body.appendChild(card2h3)
+document.body.appendChild(card2p)
+document.body.appendChild(card2a)
+document.body.appendChild(card3h3)
+document.body.appendChild(card3p)
+document.body.appendChild(card3a)
+
+firstDiv.textContent = "960 x 360px"
+firstDiv.className = "firstDiv"
+card1.textContent = "290 x 180px"
+card1.className = "cards"
+card2.className = "card2"
+card2.textContent = "290 x 180px"
+card3.className = "card3"
+card3.textContent = "290 x 180px"
+card1h3.textContent = "Indonectetus facilis"
+card1h3.className = "card1Text"
+card1p.textContent = "lorem lorem lorem lorem lorem lorem lorem"
+card1p.className = "card1Text"
+card1a.textContent = "Read More"
+card1a.className = "card1a"
+
+
+card2h3.textContent = "Indonectetus facilis"
+card2h3.className = "card2Text"
+card2p.textContent = "lorem lorem lorem lorem lorem lorem lorem"
+card2p.className = "card2Text"
+card2a.textContent = "Read More"
+card2a.className = "card2a"
+
+card3h3.textContent = "Indonectetus facilis"
+card3h3.className = "card3Text"
+card3p.textContent = "lorem lorem lorem lorem lorem lorem lorem"
+card3p.className = "card3Text"
+card3a.textContent = "Read More"
+card3a.className = "card3a"
